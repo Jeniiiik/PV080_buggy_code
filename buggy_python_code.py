0c0d025b7810b57fa3039857e0d8f7fa11f8f98a
Jan Smid <xsmid2@fi.muni.cz>
2023-03-28 15:20:45 +0200

    Better fix

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -23,10 +23,8 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    if (urllib_version == 2):
-        exec(f"import urllib2 as urllib", globals())
-    elif(urllib_version == 3):
-        exec(f"import urllib2 as urllib", globals())
+    if (urllib_version == 2 or urllib_version == 3):
+        exec(f"import urllib{urllib_version} as urllib", globals())
     else:
         print(f"Unsuitable urllib_version: {urllib_version}")
 
